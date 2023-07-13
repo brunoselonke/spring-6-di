8878fda5f4866b2e7160d26f4bff8bb6365bd0af
selonke <bs.brunoselonke@gmail.com>
2023-07-13 20:06:58 +0100

    Test created

--- src/test/java/dev/selonke/spring/ApplicationTests.java
+++ src/test/java/dev/selonke/spring/ApplicationTests.java
@@ -1,11 +1,30 @@
 package dev.selonke.spring;
 
+import dev.selonke.spring.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class ApplicationTests {
 
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Autowired
+    MyController myController;
+
+    @Test
+    void testAutoWiredOfController(){
+        System.out.println(myController.sayHello());
+    }
+    @Test
+    void testGetControolerFromCtx(){
+        MyController myController = applicationContext.getBean(MyController.class);
+
+        System.out.println(myController.sayHello());
+    }
     @Test
     void contextLoads() {
     }
