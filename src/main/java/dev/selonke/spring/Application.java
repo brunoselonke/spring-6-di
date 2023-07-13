878f821f2b866f1142e98a8f98c552f90dee8fdf
selonke <bs.brunoselonke@gmail.com>
2023-07-13 19:58:02 +0100

    New project for dependency injection

---
+++ src/main/java/dev/selonke/spring/Application.java
@@ -0,0 +1,22 @@
+package dev.selonke.spring;
+
+import dev.selonke.spring.controllers.MyController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+        MyController controller = ctx.getBean(MyController.class);
+
+        System.out.println("In main method");
+
+        System.out.println(controller.sayHello());
+    }
+
+}
